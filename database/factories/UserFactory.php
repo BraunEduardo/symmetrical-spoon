5f6c73c146b398249978ddf026451cde6acd85b3
Eduardo Braun <eduardo_braun@solis.com.br>
2025-03-19 21:40:21 -0300

    Correção na Factory de usuário e teste de cadastro

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'cpf' => fake('pt_BR')->cpf(),
+            'birthdate' => fake()->dateTimeBetween('-80 years', '-12 years')->format('Y-m-d'),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -11,6 +11,8 @@ test('registration screen can be rendered', function () {
 test('new users can register', function () {
     $response = $this->post('/register', [
         'name' => 'Test User',
+        'cpf' => '123.456.789-09',
+        'birthdate' => now()->subYears(20)->toDateString(),
         'email' => 'test@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
